cb97aa600bd5c56d6943516de3b411a284c3ea5c
Sarah Wainscott <swainscott@tql.com>
2022-05-03 21:31:05 -0400

    Add project files.

---
+++ May3Homework/Program.cs
@@ -0,0 +1,46 @@
+﻿// Write a C# Sharp program that takes a number as input and print its multiplication table.
+/*Console.Write("Please type a number and press enter. ");
+var resp = Console.ReadLine();
+var nbr = Convert.ToInt32(resp);
+var zero = 0;
+var one = 1 * nbr;
+var two = 2 * nbr;
+var three = 3 * nbr;
+var four = 4 * nbr;
+var five = 5 * nbr;
+var six = 6 * nbr;
+var seven = 7 * nbr;
+var eight = 8 * nbr;
+var nine = 9 * nbr;
+var ten = 10 * nbr; 
+Console.WriteLine($"{nbr} * 0 = {zero}");
+Console.WriteLine($"{nbr} * 1 = {one}");
+Console.WriteLine($"{nbr} *2 = {two}");
+Console.WriteLine($"{nbr} *3 = {three}");
+Console.WriteLine($"{nbr} * 4 = {four}");
+Console.WriteLine($"{nbr} * 5 = {five}");
+Console.WriteLine($"{nbr} * 6 = {six}");
+Console.WriteLine($"{nbr} * 7 = {seven}");
+Console.WriteLine($"{nbr} * 8 = {eight}");
+Console.WriteLine($"{nbr} * 9 = {nine}");
+Console.WriteLine($"{nbr} * 10 = {ten}");
+*/
+/*
+//Write a C# Sharp program to check whether two given integer values are in the range 20..50 inclusive.
+//Return true if 1 or other is in the said range otherwise false.
+var nbr1 = 10;
+var nbr2 = 40;
+
+if(nbr1 >= 20 && nbr1 <= 50 || nbr2 >= 20 && nbr2 <= 50) {
+    Console.WriteLine( "true");
+    }
+else {
+    Console.WriteLine("false");
+    }
+*/
+/*triangle
+Console.WriteLine("  *  ");
+Console.WriteLine(" *** ");
+Console.WriteLine("*****");
+*/
+
